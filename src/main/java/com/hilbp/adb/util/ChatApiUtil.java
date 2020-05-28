1a734831ddb002fbe24e64aa6a8c8fac592bf234
hilbp <1435682155@qq.com>
2020-05-28 10:49:53 +0800

    优化

--- src/main/java/com/hilbp/adb/util/ChatApiUtil.java
+++ src/main/java/com/hilbp/adb/util/ChatApiUtil.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.hilbp.adb.client.SimpleBotClientService;
-import com.hilbp.adb.entity.ChatMsgEntity;
 
 @Component
 public class ChatApiUtil {
@@ -16,7 +16,8 @@ public class ChatApiUtil {
 	public String getChatMsg(String msg) {
 		
 		String jsonStr = simpleBotClientService.chatBot("free", 0, msg);
-		ChatMsgEntity chatMsgEntity = JSON.parseObject(jsonStr, ChatMsgEntity.class);
-		return chatMsgEntity.getContent();
+		JSONObject jsonObj = JSON.parseObject(jsonStr);
+		String content = jsonObj.getString("content");
+		return content;
 	}
 }
