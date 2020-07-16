d70a99edbb5129f32d6947162ca19b63fbb2ad0d
hilbp <1435682155@qq.com>
2020-07-16 14:07:32 +0800

    test

--- src/main/java/com/hilbp/adb/action/type2/entity/Impl/BackEntity.java
+++
@@ -1,7 +0,0 @@
-package com.hilbp.adb.action.type2.entity.Impl;
-
-import com.hilbp.adb.action.type2.entity.ActionEntity;
-import lombok.Data;
-
-@Data
-public class BackEntity extends ActionEntity {}
